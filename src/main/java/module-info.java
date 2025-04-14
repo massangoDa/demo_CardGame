beda9b35f662c68c37220f119b6a04ce693b9de0
massangoDa <massango0303@outlook.jp>
2025-04-14 12:11:12 +0900

    change

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module jp.massango.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens jp.massango.demo to javafx.fxml;
+    exports jp.massango.demo;
+}
