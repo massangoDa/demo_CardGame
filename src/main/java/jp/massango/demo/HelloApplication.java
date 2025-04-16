2cda8aa8979d2f629be5bf918ea589ec8907f3e9
massangoDa <massango0303@outlook.jp>
2025-04-16 11:57:43 +0900

    start-menuを追加。 画面遷移を作成。

--- src/main/java/jp/massango/demo/HelloApplication.java
+++ src/main/java/jp/massango/demo/HelloApplication.java
@@ -8,13 +8,30 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        primaryStage = stage;
+        loadStartMenu();
+    }
+
+    private void loadStartMenu() throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartMenu.class.getResource("start-menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1208, 760);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("StartMenu");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    static void loadHelloView() throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartMenu.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1208, 760);
+        primaryStage.setTitle("HelloView");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
